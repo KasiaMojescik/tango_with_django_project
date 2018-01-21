7a624b12a650b7cf3235183ae12a34251f65eb5c
Kasia Mojeścik <Kasia>
2018-01-21 17:07:08 +0000

    correct all commit after chapter 5

---
+++ rango/urls.py
@@ -0,0 +1,8 @@
+from rango import views
+from django.conf.urls import url
+from django.contrib import admin
+
+urlpatterns = [
+url(r'^$', views.index, name='index'),
+url(r'^admin/', admin.site.urls),
+]
