1b161f9e397e962372d8cf25ecdbfa4f8f138c1d
Kasia Mojeścik <Kasia>
2018-02-03 18:40:20 +0000

    chapter 6

--- rango/admin.py
+++ rango/admin.py
@@ -7,6 +7,13 @@ admin.site.register(Category)
 
 class PageAdmin(admin.ModelAdmin):
 	list_display = ('title', 'category', 'url')
+#admin.site.register(Page, PageAdmin)
 
 	
-admin.site.register(Page, PageAdmin)
+class CategoryAdmin(admin.ModelAdmin):
+	prepopulated_fields = {'slug':('name',)}
+# Update the registration to include this customised interface
+admin.site.unregister(Category)
+admin.site.register(Category, CategoryAdmin)	
+	
+	
